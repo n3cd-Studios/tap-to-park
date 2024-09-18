78ebc25616a9b8b336ce75db95e25571a2b1230a
Struck713 <noahstruck71303@gmail.com>
2024-09-18 15:25:36 -0400

    Initial commit

---
+++ api/server.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func Server() {
+	router := gin.Default()
+	service := Service{}
+
+	api := router.Group("/api")
+	api.GET("/test", service.getAlbums) // api: /api/test
+
+	// serve static files
+	router.Use(static.Serve("/", static.LocalFile("./dist", true)))
+
+	router.Run("localhost:8080")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "tap-to-park/api"
+
+func main() {
+	api.Server()
+}
