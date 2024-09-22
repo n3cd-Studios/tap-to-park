7d98c24d4148a7f537464cab6d77d5c0f2af9635
Struck713 <noahstruck71303@gmail.com>
2024-09-22 14:43:01 -0400

    Got Leaflet working with Sveltekit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+    import Map from "$lib/Map.svelte";
 
-<h1 class="text-3xl">Hey</h1>
+</script>
+
+<h1 class="text-3xl">Yeah we got a map</h1>
+<div class="w-80 h-80">
+    <Map />
+</div>
