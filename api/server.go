41b105294206b049c3535ea82e8bed4c04a7a221
Struck713 <noahstruck71303@gmail.com>
2024-09-22 17:02:23 -0400

    Got package.json commands working

--- api/server.go
+++ api/server.go
@@ -5,15 +5,14 @@ import (
 	"os"
 	"tap-to-park/database"
 
-	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
-func Server() {
+func main() {
 
 	// Load .env file
-	err := godotenv.Load()
+	err := godotenv.Load("../.env")
 	if err != nil {
 		log.Panic("Error loading .env file")
 	}
@@ -32,7 +31,5 @@ func Server() {
 		reservations.GET("/:id", service.getReservationByID)
 	}
 
-	// serve static files
-	router.Use(static.Serve("/", static.LocalFile("./dist", true)))
-	router.Run(os.Getenv("HOST"))
+	router.Run(os.Getenv("BACKEND_HOST"))
 }
