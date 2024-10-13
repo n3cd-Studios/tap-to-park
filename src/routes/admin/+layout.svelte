28f64575c40209a347a1922b128951e438fb5781
Struck713 <noahstruck71303@gmail.com>
2024-10-12 22:13:26 -0400

    Mostly fixed mobile formatting

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -4,12 +4,12 @@ import AuthRoute from "../../components/auth/AuthRoute.svelte";
 </script>
 
 <AuthRoute>
-    <div class="flex flex-row justify-around mt-10">
-        <div class="flex flex-col gap-2 ml-10 bg-white pl-6 pr-14 py-3 rounded-lg">
+    <div class="flex flex-col justify-around mt-10 sm:flex-row gap-2">
+        <div class="flex flex-col gap-2 mx-3 sm:ml-10 bg-white pl-6 pr-14 py-3 rounded-2xl">
             <Hover href="/admin">Dashboard</Hover>
             <Hover href="/admin/invites">Invites</Hover>
         </div>
-        <div class="w-full mx-10">
+        <div class="mx-3 sm:w-full sm:mx-10">
             <slot />
         </div>
     </div>
