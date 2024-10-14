3d5776a2d0c043376e1502349b6279c031b9c480
Struck713 <noahstruck71303@gmail.com>
2024-10-14 17:28:27 -0400

    Reworked table to support links, updated admin dashboard

--- src/routes/admin/invites/+page.svelte
+++ src/routes/admin/invites/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import Table from '../../../components/Table.svelte';
+  import Table from '../../../components/table/Table.svelte';
   import { authStore } from '$lib/auth';
   import { getWithDefault } from '$lib/api';
     import type { Invite } from '$lib/models';
+    import TableItem from '../../../components/table/TableItem.svelte';
 
   let loading = true;
   let error: string;
@@ -20,4 +21,8 @@
   
 </script>
   
-<Table columns={["code", "expiration"]} {data} {loading} {error} />
+<Table columns={["code", "expiration"]} {data} {loading} {error} 
+  let:code let:expiration>
+  <TableItem>{code}</TableItem>
+  <TableItem>{expiration}</TableItem>
+</Table>
