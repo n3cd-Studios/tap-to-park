f8671076e65f35560229d23da6a6716c706bf86f
Struck713 <noahstruck71303@gmail.com>
2024-10-20 03:45:01 -0400

    Changes to toast to make it look a little better

--- src/components/toaster/Toaster.svelte
+++ src/components/toaster/Toaster.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-    import type { Toast } from "./toaster";
+    import Fa from "svelte-fa";
+import type { Toast } from "./toaster";
     import { toaster } from "./toaster";
     import { blur } from "svelte/transition";
+    import { faRemove } from "@fortawesome/free-solid-svg-icons";
 
     let toasts: Toast[] = [];
     toaster.subscribe((val) => (toasts = val));
@@ -10,18 +12,9 @@
 <div>
     <div class="absolute m-14 flex flex-col gap-1 bottom-0 text-white">
         {#each toasts as { message, type = "success" }, index}
-            <div transition:blur={{ amount: 1 }}>
-                {#if type == "success"}
-                    <div class="px-2 py-4 rounded-md bg-green-500">
-                        <button on:click={() => toaster.remove(index)}>x</button>
-                        {message}
-                    </div>
-                {:else if type == "error"}
-                    <div class="px-2 py-4 rounded-md bg-red-500">
-                        <button on:click={() => toaster.remove(index)}>x</button>
-                        {message}
-                    </div>
-                {/if}
+            <div class={`px-2 py-4 rounded-md flex gap-2 ${type == "success" ? "bg-green-500" : "bg-red-500"}`} transition:blur={{ amount: 1 }}>
+                <button class="" on:click={() => toaster.remove(index)}><Fa icon={faRemove}/></button>
+                {message}
             </div>
         {/each}
     </div>
