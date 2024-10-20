8202d8c7c06d9b08a798b76b8981a57b9cf52375
Struck713 <noahstruck71303@gmail.com>
2024-10-20 03:29:35 -0400

    Updated register page to allow for registration without code

--- src/routes/auth/register/+page.svelte
+++ src/routes/auth/register/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
     import { register } from "$lib/auth";
+    import { UserRole } from "$lib/models";
     import Button from "../../../components/form/Button.svelte";
     import Input from "../../../components/form/Input.svelte";
     import { toaster } from "../../../components/toaster/toaster";
@@ -9,9 +10,11 @@
     let password: string;
     let invite: string;
 
+    let showInvite = false;
+
     const handleRegister = async () => {
     await register(email, password, invite)
-        .then(() => goto("/auth/login"))
+        .then(user => user?.role == UserRole.ADMIN ? goto("/admin") : goto("/user"))
         .catch(() => toaster.push({ type: "error", message: "Failed to register." }, 5000));
     };
 
@@ -21,8 +24,13 @@
     <form class="p-10 bg-white rounded-xl w-1/3" on:submit|preventDefault={handleRegister}>
         <Input bind:value={email} name="Email"/>
         <Input bind:value={password} name="Password" type="password"/>
-        <Input bind:value={invite} name="Invite Code" placeholder="Invite only for now:)"/>
-        <div class="flex flex-row justify-end">
+        {#if showInvite}
+            <Input bind:value={invite} name="Invite Code" />
+        {/if}
+        <div class="flex flex-row justify-between">
+            <button type="button" class="text-blue-400" on:click={() => showInvite = !showInvite}>
+                {`I ${showInvite ? "don't":""} have an invite code`}
+            </button>
             <Button type="submit">Register</Button>
         </div>
     </form>
