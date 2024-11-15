fad97cae58a4e3e8c76455b1acb21066d871a589
Cars-n <carsonsmith8@hotmail.com>
2024-11-15 18:14:33 -0500

    Add end reservation time and button that leads to main page on success page

--- src/routes/[id]/success/+page.svelte
+++ src/routes/[id]/success/+page.svelte
@@ -2,6 +2,8 @@
     import type { Spot } from "$lib/models";
     import { onMount } from "svelte";
     import Map from "../../../components/Map.svelte";
+    import Button from "../../../components/form/Button.svelte";
+    import { goto } from "$app/navigation";
 
     export let data: Spot;
 
@@ -27,8 +29,14 @@
 <div class="flex h-full items-center justify-center">
     <div class="flex flex-col gap-2 text-center">
         <p>Successfully purchased spot!</p>
+        {#if data.reservation}
+            <p>Reservation expires at: {new Date(data.reservation.end).toLocaleString()}</p>
+        {:else}
+            <p>Reservation has expired</p>
+        {/if}
         <div class="w-96 h-96 rounded-lg border-white border-4">
             <Map bind:map={map}/>
         </div>
+        <Button on:click={() => goto("/")}>Main Page</Button>
     </div>
 </div>
