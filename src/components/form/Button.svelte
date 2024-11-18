35d1112fa6dfceee75305fec985d5c61d54ae271
Carson Smith <104526149+Cars-n@users.noreply.github.com>
2024-11-17 22:06:59 -0500

    Add improvements to spot reservation process (#131)
    * Add minutes to reservation
    
    * Add checks for invalid input on purchase page
    
    Add styles for disabled button
    
    * Change purchase page inputs to number inputs
    
    Disable number spinners on number input fields
    
    * Add end reservation time and button that leads to main page on success page

--- src/components/form/Button.svelte
+++ src/components/form/Button.svelte
@@ -5,10 +5,10 @@
   export let buttonType: ButtonType = ButtonType.DEFAULT;
 
   const buttonStyles = {
-    [ButtonType.POSITIVE]: 'bg-green-600 hover:bg-green-500',
-    [ButtonType.CAUTION]: 'bg-yellow-600 hover:bg-yellow-500',
-    [ButtonType.NEGATIVE]: 'bg-red-600 hover:bg-red-500',
-    [ButtonType.DEFAULT]: 'bg-gray-600 hover:bg-gray-500',
+    [ButtonType.POSITIVE]: 'bg-green-600 hover:bg-green-500 disabled:bg-gray-300 disabled:text-gray-600',
+    [ButtonType.CAUTION]: 'bg-yellow-600 hover:bg-yellow-500 disabled:bg-gray-300 disabled:text-gray-600',
+    [ButtonType.NEGATIVE]: 'bg-red-600 hover:bg-red-500 disabled:bg-gray-300 disabled:text-gray-600',
+    [ButtonType.DEFAULT]: 'bg-gray-600 hover:bg-gray-500 disabled:bg-gray-300 disabled:text-gray-600',
   }
 </script>
 
--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -16,6 +16,8 @@
     const costPerMinute = costPerHour / 60;
     $: cost = (costPerHour * hours) + (costPerMinute * minutes);
 
+    $: purchaseDisabled = (hours == 0 && minutes == 0) || (hours < 0 || minutes < 0) || (Number(hours)*60 + Number(minutes) > data.maxHours*60) || (cost < 0.50);
+
     const checkout = async () => {
         const session = await get<{ url: string }>({ route: `stripe/${data.guid}`, method: "POST", body: {
             start: moment().toISOString(),
@@ -33,18 +35,27 @@
             <p>Claim this space for:</p>
             <div class="flex flex-col gap-2">
                 <div class="flex flex-row gap-2 items-baseline text-4xl">
-                    <input bind:value={hours} class="bg-white rounded-lg text-black text-center w-1/4 py-5" max="24"  />
+                    <input bind:value={hours} class="bg-white rounded-lg text-black text-center w-1/4 py-5" type="number" inputmode="numeric" max="24"  />
                     <p>{hours == 1 ? "hour" : "hours"}</p>
                 </div>
                 <div class="flex flex-row gap-2 items-baseline text-4xl">
-                    <input bind:value={minutes} class="bg-white rounded-lg text-black text-center w-1/4 py-5" max="60" />
+                    <input bind:value={minutes} class="bg-white rounded-lg text-black text-center w-1/4 py-5" type="number" inputmode="numeric" max="60" />
                     <p>{minutes == 1 ? "minute" : "minutes"}</p>
                 </div>
             </div>
-            <p class="text-4xl">Total: {Formats.USDollar.format(cost)}</p>
+            {#if (hours == 0 && minutes == 0) || (hours < 0 || minutes < 0)}
+                <p class="text-4xl">Enter a valid time</p>
+            {:else if (Number(hours)*60 + Number(minutes) > data.maxHours*60)}
+                <p class="text-4xl">Time exceeds the spot's maximum of {data.maxHours} hours</p>
+            {:else}
+                <p class="text-4xl">Total: {Formats.USDollar.format(Math.floor(cost*100)/100)}</p>
+                {#if cost < 0.50}
+                    <p class="text-2xl">Cost must be at least 50¢ to purchase</p>
+                {/if}
+            {/if}
         </div>
         <div class="mb-10">
-            <Button on:click={() => checkout()}>Purchase</Button>
+            <Button on:click={() => checkout()} disabled={purchaseDisabled}>Purchase</Button>
         </div>
     </div>
 {:else}
@@ -67,7 +78,7 @@
                     <p class="text-7xl font-bold">{Formats.USDollar.format(costPerHour)}</p>
                     <p>/hour</p>
                 </div>
-                <p>Maximum time: <span class="text-black">{pluralize(2, "hour")}</span></p>
+                <p>Maximum time: <span class="text-black">{pluralize(data.maxHours, "hour")}</span></p>
             </div>
             <div class="mb-10">
                 <Button on:click={() => continued = true}>Continue</Button>
